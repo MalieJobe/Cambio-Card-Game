d1ee20c82d69533df49c5c9ec9473d6a909d4c66
MalieJobe <maliejobe@outlook.de>
2024-07-06 10:14:53 +0200

    Add initial states/hands & communicate them to clients

--- client/src/components/CambioGame.vue
+++ client/src/components/CambioGame.vue
@@ -2,30 +2,29 @@
 import { io } from "socket.io-client"
 import { onMounted, ref } from "vue";
 
-
 const socket = ref(null);
-const context = ref({});
-const position = ref({
-  x: 0,
-  y: 0,
-})
-
-const game = ref(null);
-
 socket.value = io('http://localhost:3210');
 
+const activeDeck = ref(null)
+const discardedDeck = ref(null)
+const myDeck = ref()
 
 
 onMounted(() => {
-  context.value = game.value.getContext('2d');
-  socket.value.on("position", data => {
-    position.value = data;
-    context.value.clearRect(0, 0, game.value.width, game.value.height);
-    context.value.fillRect(position.value.x, position.value.y, 20, 20);
+  socket.value.on("initialState", data => {
+    activeDeck.value = data.activeDeck;
+    discardedDeck.value = data.discardedDeck;
+    myDeck.value = data.myDeck;
+
+    console.log(data.activeDeck, data.myDeck);
+  })
+
+  socket.value.on("error", data => {
+    console.error(data.message)
   })
 });
 
-function move(direction) {
+function drawCard(direction) {
   socket.value.emit("move", direction)
 }
 
@@ -33,13 +32,9 @@ function move(direction) {
 </script>
 
 <template>
-  <canvas ref="game" width="400" height="400" style="border:1px solid black"></canvas>
-  <p>
-    <button @click="move('left')">left</button>
-    <button @click="move('right')">right</button>
-    <button @click="move('up')">up</button>
-    <button @click="move('down')">down</button>
-  </p>
+  <div class="my_hand">
+    <p v-for="card in myDeck">{{ card }}</p>
+  </div>
 </template>
 
 <style scoped></style>
